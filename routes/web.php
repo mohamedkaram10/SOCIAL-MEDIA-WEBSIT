1dd4a4f3e8f6b7966ec9d56c173b579a42928df1
mohamedkaram10 <mohamedkaram59999@gmail.com>
2024-02-02 21:05:57 +0200

    Implement user profile page UI

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,13 @@ Route::get('/', [HomeController::class, 'index'])
 ->middleware(['auth', 'verified'])
 ->name('home');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+Route::get('/u/{user:username}', [ProfileController::class, 'index'])
+->name('profile');
+
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
 
 require __DIR__.'/auth.php';
