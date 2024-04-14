459c20f3e62249b5a9867b4160596a04d15ca8cc
Mohamed Karam <mohamed.karam@id3m.com>
2024-04-14 17:59:13 +0200

    Set fixed height on comments section

--- resources/js/Components/app/InputTextarea.vue
+++ resources/js/Components/app/InputTextarea.vue
@@ -50,7 +50,8 @@ onMounted(() => {
 
 <template>
     <textarea
-        class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm"
+        class="border-gray-300 dark:border-gray-700 h-10 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm"
+        style="height: 40px;"
         :value="modelValue"
         @input="onInputChange"
         ref="input"
